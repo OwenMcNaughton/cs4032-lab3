0a58839a823f9edadd2419762766f39fc913e973
OwenMcNaughton <owen.mcnaughton@gmail.com>
2016-11-17 18:36:09 +0000

    working...

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from concurrent.futures import ThreadPoolExecutor
+import collections
+from hashlib import md5
+import os
+import Queue
 import socket
 import sys
+import threading
 
 
-rooms = dict()
-room_ids = dict()
+rooms = collections.OrderedDict()
 client_ids = dict()
 client_count = 0
 room_count = 0
@@ -14,86 +17,108 @@ sock.bind(('0.0.0.0', int(sys.argv[1])))
 
 sock.listen(1000)
 
+class ThreadHandler(threading.Thread):
+    def __init__(self, thread_queue):
+        threading.Thread.__init__(self)
+        self.queue = thread_queue
+
+    def run(self):
+        # Thread loops and waits for connections to be added to the queue
+        while True:
+            conaddr = self.queue.get()
+            handler(conaddr)
+            self.queue.task_done()
+
 def announce(room_id, msg):
-    for connection in rooms[room_id]:
+    for client_id, connection in rooms[room_id].iteritems():
         connection.sendall(msg)
 
-def handler(connection, address):
+def handler((connection, address)):
     while connection:
-        msg = str(connection.recv(10000), 'utf-8');
+        msg = connection.recv(10000).decode('utf-8')
+        print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+        if 80806323619409305465361856931992828404 in rooms:
+            print(rooms[80806323619409305465361856931992828404].keys())
+        if 231755500800015387352945186706554456156 in rooms:
+            print(rooms[231755500800015387352945186706554456156].keys())
 
         if msg.startswith("HELO"):
-            response = msg + "\nIP:" + address[0] + "\nPORT:" + address[1] + \
+            response = msg + "\nIP:" + str(address[0]) + "\nPORT:" + str(address[1]) + \
                 "\nStudentID:12308914"
             connection.sendall(response)
-        if msg.startswith("KILL_SERVICE"):
-            kill_serv(connection)
 
 
-        if msg.startswith("JOIN_CHATROOM"):
+        elif msg.startswith("KILL_SERVICE"):
+            os._exit(1)
+
+
+        elif msg.startswith("JOIN_CHATROOM"):
             msg  = msg.split("\n")
             room = msg[0].split(":")[1]
             client_name = msg[3].split(":")[1]
-            if client_name not in client_ids.keys():
-                client_count += 1
-                client_ids[client_name] = client_count
-            client_id = client_ids[client_name]
-            room_id = -1
-            if room in rooms:
-                room_id = rooms[room]
-            else:
-                room_count += 1
-                room_id = room_count
-                rooms[room_ids] = dict()
-            if client_id not in rooms[room_ids[room]]:
+            room_id = int(md5(room).hexdigest(), 16)
+            client_id = int(md5(client_name).hexdigest(), 16)
+            if room_id not in rooms:
+                rooms[room_id] = dict()
+            if client_id not in rooms[room_id]:
+                print(str(client_id) + "  join  " + str(room_id))
                 rooms[room_id][client_id] = connection
-                response = "JOINED_CHATROOM:" + room + "\nSERVER_IP:10.62.0.156" +\
-                    "\nPORT:8000\nROOM_REF:" + room_id + "JOIN_ID:\n" +\
-                    client_id
+                response = "JOINED_CHATROOM:" + str(room) + "\nSERVER_IP:10.62.0.156" +\
+                    "\nPORT:8000\nROOM_REF:" + str(room_id) + "\nJOIN_ID:" +\
+                    str(client_id) + "\n"
                 connection.sendall(response)
-                joined = "CHAT:" + room_id + "\nCLIENT_NAME:" + client +\
-                    "\nMESSAGE:" + client_name + "has joined the chat room.\n\n"
+                joined = "CHAT:" + str(room_id) + "\nCLIENT_NAME:" + str(client_name) +\
+                    "\nMESSAGE:" + str(client_name) + " has joined this chatroom.\n\n"
                 announce(room_id, joined)
 
 
-        if msg.startswith("LEAVE_CHATROOM"):
+        elif msg.startswith("LEAVE_CHATROOM"):
             msg  = msg.split("\n")
             room_id = int(msg[0].split(":")[1])
-            client_id = int(msg[2].split(":")[1])
-            client_name = msg[3].split(":")[1]
-            left = "LEFT_CHATROOM:" + room_id + "\nJOIN_ID:" + client_id + "\n"
+            client_id = int(msg[1].split(":")[1])
+            client_name = msg[2].split(":")[1]
+            print(str(client_id) + "  leave  " + str(room_id))
+            left = "LEFT_CHATROOM:" + str(room_id) + "\nJOIN_ID:" + str(client_id) + "\n"
             connection.sendall(left)
-            left = "CHAT:" + room_id + "\nCLIENT_NAME:" + client_name +\
-                "\nMESSAGE:" + client_name + "has left the chat room.\n\n"
+            left = "CHAT:" + str(room_id) + "\nCLIENT_NAME:" + str(client_name) +\
+                "\nMESSAGE:" + str(client_name) + " has left this chatroom.\n\n"
             announce(room_id, left)
             del rooms[room_id][client_id]
 
 
-        if msg.startswith("DISCONNECT"):
+        elif msg.startswith("DISCONNECT"):
             msg  = msg.split("\n")
             client_name = msg[2].split(":")[1]
-            client_id = client_ids[client_name]
+            client_id = int(md5(client_name).hexdigest(), 16)
+            print("DISC  " + str(client_id))
             for room_id in rooms.keys():
-                del rooms[room_id][client_id]
                 if client_id in rooms[room_id]:
-                    left = "CHAT:" + room_id + "\nCLIENT_NAME:" + client_name +\
-                        "\nMESSAGE:" + client_name + "has left the chat room.\n\n"
+                    left = "CHAT:" + str(room_id) + "\nCLIENT_NAME:" + str(client_name) +\
+                        "\nMESSAGE:" + str(client_name) + " has left this chatroom.\n\n"
+                    print("LEAVE " + str(room_id))
+                    announce(room_id, left)
+                    if client_id in rooms[room_id]:
+                        del rooms[room_id][client_id]
             connection = False
 
 
-        if msg.startswith("CHAT"):
+        elif msg.startswith("CHAT"):
             msg  = msg.split("\n")
             room_id = int(msg[0].split(":")[1])
             client_id = int(msg[1].split(":")[1])
             client_name = msg[2].split(":")[1]
             msg = msg[3].split(":")[1]
-            msg = "CHAT:" + room_id + "\nCLIENT_NAME:" +\
-                client_name + "\nMESSAGE:" + msg
+            msg = "CHAT:" + str(room_id) + "\nCLIENT_NAME:" +\
+                str(client_name) + "\nMESSAGE:" + msg + "\n\n"
             announce(room_id, msg)
 
+queue = Queue.Queue(maxsize=1000)
 
+for i in range(1000):
+    thread = ThreadHandler(queue)
+    thread.setDaemon(True)
+    thread.start()
 
-executor = ThreadPoolExecutor(max_workers=1000)
 while True:
     connection, address = sock.accept()
-    executor.submit(handler, connection, address)
+    queue.put((connection, address))
